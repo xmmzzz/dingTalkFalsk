919210b02e2e016c12325b69a197d891f3afa3df
xmmzzz1996 <10384216+xmmzzz1996@user.noreply.gitee.com>
2023-06-03 20:15:29 +0800

    add SSL

--- hello.py
+++ hello.py
@@ -30,4 +30,4 @@ def OAHello():
     return json.dumps(OAHandler.OARobot.sayHello(req))
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 1213)
+    app.run(host = '0.0.0.0', port = 1213, ssl_context=('xmmzzz.top.pem', 'xmmzzz.top.key'))
