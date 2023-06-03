48408fa64945a2fdfb9bb877eec0ef7aeb36bdec
xmmzzz1996 <10384216+xmmzzz1996@user.noreply.gitee.com>
2023-06-03 14:51:58 +0800

    add redis to record history

--- OAHandler/OARobot.py
+++ OAHandler/OARobot.py
@@ -13,8 +13,10 @@ def sayHello(req):
         his_re = json.loads(his_re)
     res = {}
     res["msgtype"] = "text"
-    res["text"] = {"content": "收到消息: " + req["text"]["content"] + "\n"
-                   + "历史对话(3min以内): " + json.dumps(his_re)}
+    text = req["text"]["content"] + "\n"+ "历史对话(3min以内): "
+    for i in range(len(his_re)):
+        text += "{}\n".format(','.join(his_re[i]))
+    res["text"] = {"text": {"content": text}}
     his_re.append([req["text"]["content"]])
-    redisClient.set(staffID, json.dumps(his_re))
+    redisClient.set(staffID, json.dumps(his_re).encode('gbk'))
     return res
